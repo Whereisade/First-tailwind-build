e367784a4dfa9571c240d697ad215a2704e50d30
Whereisade <eniolaadeleke2303@gmail.com>
2024-09-16 14:21:02 +0100

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./build/*.{html,js}"],
-  theme: {
-    extend: {},
+  theme: { 
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
+}
